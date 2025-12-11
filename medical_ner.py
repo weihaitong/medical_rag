bbc1df22a76174970d3554c8986d6ebdc4203c35
weihaitong <1684088343@qq.com>
2025-12-11 20:51:00 +0800

    nothing

--- medical_ner.py
+++ medical_ner.py
@@ -97,3 +97,17 @@ class NER:
             entities.append(current_entity)
 
         return entities
+
+#使用例子https://huggingface.co/lixin12345/chinese-medical-ner
+# ner_model = NER('lixin12345/chinese-medical-ner')
+# text = """
+# 患者既往慢阻肺多年;冠心病史6年，平素规律服用心可舒、保心丸等控制可;双下肢静脉血栓3年，保守治疗效果可;左侧腹股沟斜疝无张力修补术后2年。否认"高血压、糖尿病"等慢性病病史，否认"肝炎、结核"等传染病病史及其密切接触史，否认其他手术、重大外伤、输血史，否认"食物、药物、其他"等过敏史，预防接种史随社会。
+# """
+# ans = ner_model.ner(text)
+# # ans
+#
+# # DiseaseNameOrComprehensiveCertificate
+# # 慢阻肺
+#
+# # DiseaseNameOrComprehensiveCertificate
+# # 冠心病
